0520286a874e824f947a751218e5c80b4b330045
elorm gershon <alidugershon@gmail.com>
2025-07-22 00:30:44 +0000

    first section done

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+   <div>
+    <h1 className="text-center text-4xl font-bold text-orange-700 my-8">Welcome Page</h1>
+    <p className="text-center text-lg text-gray-600 mb-4">
+      This is the home page of the ALX project 0x02-setup. 
+      <br />
+      The project is designed to help you set up your development environment.
+    </p>
+   </div>
+  );
+}
