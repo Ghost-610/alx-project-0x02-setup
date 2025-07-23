38f73de622c776605147ff5cebdb5c5b59c7c3ff
elorm gershon <alidugershon@gmail.com>
2025-07-23 19:45:59 +0000

    new files added

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,18 @@
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ size, shape, children, onClick }) => {
+  const sizeClasses = {
+    small: "px-2 py-1 text-sm",
+    medium: "px-4 py-2 text-base",
+    large: "px-6 py-3 text-lg",
+  };
+
+  const className = `bg-blue-400 text-white ${sizeClasses[size]} ${shape} hover:bg-blue-700 transition`;
+
+  return (
+    <button className={className} onClick={onClick} type="button">
+      {children}
+    </button>
+  );
+};
+export default Button;
---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {  type CardProps } from '@/interfaces';
+
+const Card: React.FC<CardProps> = ({ title, content }) => {
+  return (
+    <div className="bg-blue-400 shadow-md rounded-lg p-4 max-w-sm mx-auto">
+     <h1>{title}</h1>
+     <p>{content}</p>
+    </div>
+  );
+};
+export default Card;
