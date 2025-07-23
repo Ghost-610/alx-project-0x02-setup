b15789039cc5d008e4b4fc30fbb7d859fb5da074
elorm gershon <alidugershon@gmail.com>
2025-07-23 20:14:39 +0000

    bug fixed

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,6 +1,6 @@
 // import { AboutProps } from "@/interfaces";
 import Button from "@/components/common/Button";
-import Header from "../components/layout/Header";
+import Header from '@/components/layout/Header';
 
 const About: React.FC = () => {
   return (
